47ea96ff66eb631c9e464486530e5e9fecb46928
David Pham <email@davidpham.ca>
2018-07-26 19:21:23 -0400

    Success login page.

---
+++ Login/login.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyCDl5786d2qbt1J1QsOdhYVLM7o19JRoGA',
+  authDomain: 'gpa-d-7c696.firebaseapp.com',
+  databaseURL: 'https://gpa-d-7c696.firebaseio.com',
+  projectId: 'gpa-d-7c696',
+  storageBucket: 'gpa-d-7c696.appspot.com',
+  messagingSenderId: '718824836495'
+};
+firebase.initializeApp(config);
